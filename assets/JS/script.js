4bb516e0920d1e9223cf7c9a68a495b0a34de262
jaycee808 <jamie.clark@live.co.uk>
2023-04-02 21:05:00 +0100

    added function to load search when page loads

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,12 +1,64 @@
 var searchFormEl = $('#search-form');
 // store API Keys as variables
-  // EU Key
+// EU Key
 var EUAPIKey =  "tatityas";
-  // GG Key
+// GG Key
 var GGAPIKey = "AIzaSyADX0Zd0LOToDJv9h85TLZXc8GUliRPzTU";
+//
+const API_URL = "https://api.europeana.eu/record/v2/search.json?wskey=" + EUAPIKey + "&query=rembrandt"
 
+Window.onload = resultsOnload();
 
-   // create a function to search EU API when keyword is entered into search bar
+// Function to load search result on page load
+function resultsOnload() {
+
+  var onloadURL = "https://api.europeana.eu/record/v2/search.json?wskey=" + EUAPIKey + "&query=rembrandt";
+  
+  // initialise item data object to store response data 
+  var itemData = {};
+  $.ajax({
+      url: onloadURL,
+      method: "GET"
+    }).then(function(response) {
+      // Extract the item data from the API response
+      // variable of results to be filtered
+      console.log(response);
+      var items = response.items;
+    
+      // select specific data from the returned objects
+      for (var i = 0; i < items.length; i++) {
+        var item = items[i];
+        // Store the specific aspect of the item data in the object
+        // if response is an array, only return the first item from the array
+        itemData[i] = {
+          // Select Title *
+          title: Array.isArray(item.title) ? item.title[0] : item.title,
+          // Select link for provider *
+          link: Array.isArray(item.edmIsShownAt) ? item.edmIsShownAt[0] : item.edmIsShownAt,
+          // Select description *
+          description: Array.isArray(item.dcDescription) ? item.dcDescription[0] : item.dcDescription,
+          // select Year
+          year: Array.isArray(item.year) ? item.year[0] : item.year,
+          // select country
+          country: Array.isArray(item.country) ? item.country[0] : item.country,
+          // Select thumbnail image *
+          image: Array.isArray(item.edmPreview) ? item.edmPreview[0] : item.edmPreview,
+          // select location
+          // Latitude
+          latitude: Array.isArray(item.edmPlaceLatitude) ? item.edmPlaceLatitude[0] : item.edmPlaceLatitude,
+          // Longitude
+          longitude: Array.isArray(item.edmPlaceLongitude) ? item.edmPlaceLongitude[0] : item.edmPlaceLongitude,
+          // select provider
+          provider: Array.isArray(item.dataProvider)? item.dataProvider[0] : item.dataProvider,
+        };
+      }
+      console.log(response)
+      console.log(itemData);
+      displayCards(itemData);
+    });
+}
+
+// Function to search EU API when keyword is entered into search bar
 function searchEuropeana(event) {
   event.preventDefault();
 
